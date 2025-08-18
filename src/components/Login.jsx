2d6a3d3465ebc7e2940f32e217dd39a34cc5ce56
nishat-arif <nishatarif1912@gmail.com>
2025-08-18 18:01:09 -0400

    added signup feature

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import axios from 'axios';
-import {login_api_url} from '../utils/constants'
+import {login_api_url ,base_url , signup_api_suffix} from '../utils/constants'
 import { useDispatch } from "react-redux";
 import { addUser } from "../utils/store/userSlice";
 import { useNavigate } from "react-router-dom";
@@ -9,9 +9,12 @@ import {addHeaderFooter} from "../utils/store/bodySlice.js"
 
 const Login = () => {
 
+    const [firstName , setFirstName] = useState("");
+    const [lastName , setLastName] = useState("");
     const [emailId , setEmailId] = useState("");
     const [password , setPassword] = useState("");
     const [errorMessage , setErrorMessage] = useState(null)
+    const [isLoginPage , setIsLoginPage] = useState(true)
 
     const dispatch = useDispatch();
     const navigate = useNavigate();
@@ -53,18 +56,66 @@ const Login = () => {
         
      }
 
+     const handleSignUpClick = async () => {
+
+        try{
+        const signUpCredentials = {firstName , lastName , emailId , password}
+
+        const options = {withCredentials :true}
+        const signUpData = await axios.post(base_url + signup_api_suffix , signUpCredentials , options)
+
+        // const requestOptions = {
+        //                         method: 'POST',
+        //                         headers: {'Content-Type': 'application/json'},
+        //                         body: JSON.stringify(loginCredentials)};
+
+        // const data = await fetch(login_api_url, requestOptions)
+        // const loginData = await data.json();
+
+        dispatch(addUser(signUpData?.data?.data));
+        dispatch(addHeaderFooter(true))
+        navigate("/profile")
+        }
+        catch(error){
+
+            if (axios.isAxiosError(error)) {
+                // Handle Axios-specific errors (e.g., network errors, server responses with status codes)
+                console.error('Axios Error:', error.response ? error.response.data : error.message);
+                setErrorMessage("Invalid Data !!")
+                } else {
+                // Handle other types of errors
+                console.error('General Error:', error);
+                }
+
+        }
+
+        
+     }
+
 
     return (
     <div className = "flex my-30 items-center justify-center">
         <div className="card bg-primary text-primary-content w-96">
             <div className="card-body">
-                <h2 className="card-title flex justify-center text-3xl">Login</h2>
+                <h2 className="card-title flex justify-center text-3xl">{isLoginPage ? "Login" : "Sign Up"}</h2>
                 <div>
                     {errorMessage && 
                         <div role="alert" className="alert w-[95%] my-4 py-2">
                             <span>Error :  {errorMessage}</span>
                         </div>
                     }
+                    {!isLoginPage && 
+                    <>
+                        <fieldset className="fieldset my-2">
+                            <legend className="fieldset-legend">First Name</legend>
+                            <input type="text" className="input" placeholder="Type here"  value= {firstName} onChange={(e)=>{setFirstName(e.target.value)}}/>
+                        </fieldset>
+
+                        <fieldset className="fieldset">
+                            <legend className="fieldset-legend">Last Name</legend>
+                            <input type="text" className="input" placeholder="Type here"  value= {lastName} onChange={(e)=>{setLastName(e.target.value)}}/>
+                        </fieldset>
+                    </>}
                     <fieldset className="fieldset my-2">
                         <legend className="fieldset-legend">Email Id</legend>
                         <input type="text" className="input" placeholder="Type here"  value= {emailId} onChange={(e)=>{setEmailId(e.target.value)}}/>
@@ -72,11 +123,15 @@ const Login = () => {
 
                     <fieldset className="fieldset">
                         <legend className="fieldset-legend">Password</legend>
-                        <input type="text" className="input" placeholder="Type here"  value= {password} onChange={(e)=>{setPassword(e.target.value)}}/>
+                        <input type={isLoginPage ? "password" : "text"} className="input" placeholder="Type here"  value= {password} onChange={(e)=>{setPassword(e.target.value)}}/>
                     </fieldset>
                 </div>
                 <div className="card-actions justify-center my-5 ">
-                <button className="btn text-xl" onClick= {handleLoginClick}>Login</button>
+                <button className="btn text-xl" onClick= {isLoginPage ? handleLoginClick : handleSignUpClick}>{isLoginPage ? "Login" : "Sign Up"}</button>
+                </div>
+                <div >
+                <p className="flex justify-center text-md cursor-pointer" onClick={()=>setIsLoginPage(!isLoginPage)}>
+                    {isLoginPage ? "New User ? Sign up here" : "Already registered? Login here"}</p>
                 </div>
             </div>
             </div>
